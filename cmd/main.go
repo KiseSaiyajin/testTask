ddfe561d1e494f56ccf4197a9b22dbefad3eccb8
Rafael <trafsel2000@gmail.com>
2024-09-06 11:48:06 +0500

    Add Postgres migration up and down

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,25 @@ import (
 	"github.com/KiseSaiyajin/testTask/pkg/handler"
 	"github.com/KiseSaiyajin/testTask/pkg/repository"
 	"github.com/KiseSaiyajin/testTask/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error in initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	serv := new(testtask.Server)
-	if err := serv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := serv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error ocurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
